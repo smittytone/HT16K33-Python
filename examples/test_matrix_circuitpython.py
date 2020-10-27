a66d08d315de3a79842a133403eb6bdaeb988c2c
Tony Smith <smittytone@icloud.com>
2020-10-27 15:32:52 +0000

    Latest upload @ Tue 27 Oct 2020 15:32:52 GMT

--- examples/test_matrix_circuitpython.py
+++ examples/test_matrix_circuitpython.py
@@ -3,7 +3,7 @@ import time
 import board
 import busio
 from random import randint
-from ht16k33matrixfeatherwing import HT16K33MatrixFeatherWing
+from ht16k33matrix import HT16K33Matrix
 
 # CONSTANTS
 DELAY = 0.01
@@ -14,46 +14,39 @@ if __name__ == '__main__':
     i2c = busio.I2C(board.SCL, board.SDA)
     while not i2c.try_lock():
         pass
-    display = HT16K33MatrixFeatherWing(i2c)
+    display = HT16K33Matrix(i2c)
     display.set_brightness(2)
 
-    sync_text = "ABC"
-    col = 0
-    for i in range(len(sync_text)):
-        display.set_character(ord(sync_text[i]), col)
-        col += 6
-    display.draw()
-
-    time.sleep(PAUSE)
-    display.clear().draw()
-
     icon = b"\x3C\x42\xA9\x85\x85\xA9\x42\x3C"
-    display.set_icon(icon, 4).draw()
+    display.set_icon(icon).draw()
+
+    time.sleep(PAUSE)
+    display.set_angle(900).draw()
 
     time.sleep(PAUSE)
     display.clear().draw()
 
-    icon = b"\x00\x00\x0E\x18\xBE\x6D\x3D\x3C"
+    icon = b"\x0E\x18\xBE\x6D\x3D\x3C"
     display.define_character(icon, 0)
-    icon = b"\x3C\x3D\x6D\xBE\x18\x0E\x00\x00"
+    icon = b"\x3C\x3D\x6D\xBE\x18\x0E"
     display.define_character(icon, 1)
-    display.set_character(0, 0).set_character(1, 8).draw()
 
-    time.sleep(PAUSE)
-    display.clear().draw()
-
-    text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 abcdefghijklmnopqrstuvwxyz !$%&*() \x00\x01"
+    text = "0123456789 abcdefghijklmnopqrstuvwxyz !$%&*() \x00\x01"
     display.scroll_text(text)
 
     time.sleep(PAUSE)
+    display.set_character(0, True).draw()
+
+    time.sleep(PAUSE)
+    display.set_inverse().draw()
+
+    time.sleep(PAUSE)
+    display.set_inverse().draw()
+
+    time.sleep(PAUSE)
     display.clear().draw()
 
-    for i in range(64):
-        while True:
-            x = randint(0, 15)
-            y = randint(0, 7)
-            if not display.is_set(x, y): break
-        display.plot(x, y).draw()
-        time.sleep(0.5)
-
+    for i in range(4):
+        display.plot(i, i).plot(7 - i, i).plot(i, 7 - i).plot(7 - i, 7 - i)
+    display.draw()
     display.set_blink_rate(1)
