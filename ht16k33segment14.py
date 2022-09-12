851dac33f5b3024bb2ea22dd4be9d1c934cba4d8
Tony Smith <smittytone@icloud.com>
2022-09-12 12:27:17 +0100

    Add comments; update method docs; code tweaks; charset tweaks

--- ht16k33segment14.py
+++ ht16k33segment14.py
@@ -1,11 +1,11 @@
-# Import the base class       # note this is akb modified version
+# Import the base class
 from ht16k33 import HT16K33
 
 class HT16K33Segment14(HT16K33):
     """
     Micro/Circuit Python class for the Adafruit 0.54in Quad Alphanumeric Display,
     a four-digit, 14-segment LED displays driven by the HT16K33 controller
-    Version:    3.3.0
+    Version:    3.3.1
     Bus:        I2C
     Author:     Tony Smith (@smittytone)
     License:    MIT
@@ -28,8 +28,8 @@ class HT16K33Segment14(HT16K33):
     HT16K33_SEG14_CHAR_COUNT  = 76
 
     # CHARSET store character matrices for 0-9, A-Z, a-z, space and various symbols
-    CHARSET = b'\x00\x3F\x00\x06\x00\xDB\x00\x8F\x00\xE6\x00\xED\x00\xFD\x00\x07\x00\xFF\x00\xEF\x00\xF7\x12\x8F\x00\x39\x12\x0F\x00\x79\x00\x71\x00\xBD\x00\xF6\x12\x09\x00\x1E\x0C\x70\x00\x38\x05\x36\x09\x36\x00\x3F\x00\xF3\x08\x3F\x08\xF3\x00\xED\x12\x01\x00\x3E\x24\x30\x28\x36\x2D\x00\x15\x00\x24\x09\x10\x58\x08\x78\x00\xD8\x20\x8E\x20\x58\x24\x80\x04\x8E\x10\x70\x10\x00\x08\x06\x1E\x00\x20\x30\x10\xD4\x10\x50\x00\xDC\x01\x70\x04\x86\x00\x50\x08\x88\x00\x78\x00\x1C\x08\x04\x28\x14\x2D\x00\x25\x00\x20\x48\x00\x00\x00\x06\x02\x20\x10\x83\x12\xED\x24\x24\x00\xE3\x04\x00\x09\x00\x20\x00\x3F\xC0\x12\xC0\x00\xC0\x24\x00'
-# akb modified 0, 4 and 7 to be like 7 segment presentations
+    CHARSET = b'\x24\x3F\x00\x06\x00\xDB\x00\x8F\x00\xE6\x00\xED\x00\xFD\x00\x07\x00\xFF\x00\xEF\x00\xF7\x12\x8F\x00\x39\x12\x0F\x00\x79\x00\x71\x00\xBD\x00\xF6\x12\x09\x00\x1E\x0C\x70\x00\x38\x05\x36\x09\x36\x00\x3F\x00\xF3\x08\x3F\x08\xF3\x00\xED\x12\x01\x00\x3E\x24\x30\x28\x36\x2D\x00\x15\x00\x24\x09\x10\x58\x08\x78\x00\xD8\x20\x8E\x20\x58\x24\x80\x04\x8E\x10\x70\x10\x00\x08\x06\x1E\x00\x20\x30\x10\xD4\x10\x50\x00\xDC\x01\x70\x04\x86\x00\x50\x08\x88\x00\x78\x00\x1C\x08\x04\x28\x14\x2D\x00\x25\x00\x20\x48\x00\x00\x00\x06\x02\x20\x10\x83\x12\xED\x24\x24\x00\xE3\x04\x00\x09\x00\x20\x00\x3F\xC0\x12\xC0\x00\xC0\x24\x00'
+
 
     # *********** CONSTRUCTOR **********
 
@@ -55,26 +55,27 @@ class HT16K33Segment14(HT16K33):
             4 |   | 2           /|\
               | _ |         11 / | \ 13    . 14
                 3                12
-        Bit 14 is the period, but this is set with parameter 3
-        Nb. Bit 15 is not read by the display
+
+        For HT16K33-based devices, swap bits 11 and 13: ie. set bit 11
+        for a bottom right stroke, and bit 13 for a bottom left stroke.
+
+        Bit 14 is the period, but this is set with parameter 3.
+        Bit 15 is not read by the display.
 
         Args:
-            glyph (int):   The glyph pattern.
-            digit (int):   The digit to show the glyph. Default: 0 (leftmost digit).
+            glyph (int):    The glyph pattern.
+            digit (int):    The digit to show the glyph. Default: 0 (leftmost digit).
+            has_dot (bool): Should the decimal point (where available) be illuminated?
 
         Returns:
             The instance (self)
         """
-
         # Bail on incorrect row numbers or character values
         assert 0 <= digit < 4, "ERROR - Invalid digit (0-3) set in set_glyph()"
         assert 0 <= glyph < 0xFFFF, "ERROR - Invalid glyph (0x0000-0xFFFF) set in set_glyph()"
 
         # Write the character to the buffer
-        #if has_dot: glyph |= self.HT16K33_SEG14_DP_VALUE #akb
-        #self.buffer[digit << 1] = (glyph >> 8) & 0xFF #akb
-        #self.buffer[(digit << 1 + 1)] = glyph & 0xFF #akb
-        self._set_digit(glyph, digit, has_dot) #akb
+        return self._set_digit(glyph, digit, has_dot)
 
     def set_number(self, number, digit=0, has_dot=False):
         """
@@ -84,17 +85,21 @@ class HT16K33Segment14(HT16K33):
         Call 'update()' to render the buffer on the display.
 
         Args:
-            number (int):  The number to show.
-            digit (int):   The digit to show the number. Default: 0 (leftmost digit).
+            number (int):   The number to show.
+            digit (int):    The digit to show the number. Default: 0 (leftmost digit).
+            has_dot (bool): Should the decimal point (where available) be illuminated?
 
         Returns:
             The instance (self)
         """
+        # Bail on incorrect row numbers or character values
         assert 0 <= digit < 4, "ERROR - Invalid digit (0-3) set in set_number()"
         assert 0 <= number < 10, "ERROR - Invalid value (0-9) set in set_number()"
-        return self.set_character(str(number), digit, has_dot) #akb
 
-    def set_character(self, char, digit=0, has_dot=False):  #akb
+        # Write the character to the buffer
+        return self.set_character(str(number), digit, has_dot)
+
+    def set_character(self, char, digit=0, has_dot=False):
         """
         Present single alphanumeric character at the specified digit.
 
@@ -107,11 +112,15 @@ class HT16K33Segment14(HT16K33):
         Args:
             char (string):  The character to show.
             digit (int):    The digit to show the number. Default: 0 (leftmost digit).
+            has_dot (bool): Should the decimal point (where available) be illuminated?
 
         Returns:
             The instance (self)
         """
+        # Bail on incorrect row number
         assert 0 <= digit < 4, "ERROR - Invalid digit set in set_character()"
+
+        # Determine the character's entry in the charset table
         char_val = 0xFFFF
         if char == '-':
             char_val = self.HT16K33_SEG14_MINUS_CHAR
@@ -133,11 +142,14 @@ class HT16K33Segment14(HT16K33):
             char_val = ord(char) - 55   # 10-35
         elif char in 'abcdefghijklmnopqrstuvwxyz':
             char_val = ord(char) - 61   # 36-61
-        assert char_val != 0xFFFF, "ERROR - Invalid char string set in set_character() " + char + " (" + str(ord(char)) + ")"
-        self._set_digit((self.CHARSET[char_val << 1] << 8) | self.CHARSET[(char_val << 1) + 1], digit, has_dot) #akb
-        return self
 
-    def set_code(self, code, digit, has_dot=False):  #alb
+        # Bail on incorrect character values
+        assert char_val != 0xFFFF, "ERROR - Invalid char string set in set_character() " + char + " (" + str(ord(char)) + ")"
+
+        # Write the character to the buffer
+        return self._set_digit((self.CHARSET[char_val << 1] << 8) | self.CHARSET[(char_val << 1) + 1], digit, has_dot)
+
+    def set_code(self, code, digit, has_dot=False):
         """
         Present single alphanumeric character at the specified digit.
 
@@ -150,20 +162,23 @@ class HT16K33Segment14(HT16K33):
         Args:
             code (int):     The character's class-specific code.
             digit (int):    The digit to show the number. Default: 0 (leftmost digit).
+            has_dot (bool): Should the decimal point (where available) be illuminated?
 
         Returns:
             The instance (self)
         """
+        # Bail on incorrect row numbers or code values
         assert 0 <= digit < 4, "ERROR - Invalid digit (0-3) set in set_code()"
-        assert 0 <= code < 76, "ERROR - Invalid code (0-{:d}) set in set_code()".format(self.HT16K33_SEG14_CHAR_COUNT - 1) #akb
-        self._set_digit((self.CHARSET[code << 1] << 8) | self.CHARSET[(code << 1) + 1], digit, has_dot) #akb
-        return self
+        assert 0 <= code < self.HT16K33_SEG14_CHAR_COUNT, "ERROR - Invalid code (0-{:d}) set in set_code()".format(self.HT16K33_SEG14_CHAR_COUNT - 1)
+
+        # Write the character to the buffer
+        return self._set_digit((self.CHARSET[code << 1] << 8) | self.CHARSET[(code << 1) + 1], digit, has_dot)
 
 
     # *********** PRIVATE FUNCTIONS (DO NOT CALL) **********
 
-    def _set_digit(self, value, digit, has_dot): #akb
-        if has_dot: value |= self.HT16K33_SEG14_DP_VALUE  #akb
+    def _set_digit(self, value, digit, has_dot=False):
+        if has_dot: value |= self.HT16K33_SEG14_DP_VALUE
         if self.is_ht16k33:
             # Output for HT16K33: swap bits 11 and 13,
             # and sequence becomes LSB, MSB
