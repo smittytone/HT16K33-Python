dfe810026dc960a03a6569e57d6ee0f4b8c17b46
Tony Smith <smittytone@icloud.com>
2022-09-12 12:27:46 +0100

    Ditto

--- examples/circuitpython/test_segement14_ht16k33.py
+++ examples/circuitpython/test_segement14_ht16k33.py
@@ -10,58 +10,60 @@ PAUSE = 3
 
 # START
 if __name__ == '__main__':
-    i2c = busio.I2C(board.SCL, board.SDA)
+    i2c = busio.I2C(scl=board.D7, sda=board.D6)
     while not i2c.try_lock():
         pass
     display = HT16K33Segment14(i2c,is_ht16k33=True)
     display.set_brightness(2)
     display.clear()
-    #display.set_digit(0x003F,3)
 
-    a = 65
-    while (a < 88):
-        display.clear()
-        display.set_character(chr(a), 0)
-        display.set_character(chr(a + 1), 1)
-        display.set_character(chr(a + 2), 2)
-        display.set_character(chr(a + 3), 3)
-        display.draw()
-        a += 1
-        time.sleep(0.5)
-    time.sleep(5)
+    point_state = True
+    while True:
+        a = 65
+        while (a < 88):
+            display.clear()
+            display.set_character(chr(a), 0, point_state)
+            display.set_character(chr(a + 1), 1, point_state)
+            display.set_character(chr(a + 2), 2, point_state)
+            display.set_character(chr(a + 3), 3, point_state)
+            display.draw()
+            a += 1
+            time.sleep(0.5)
+        time.sleep(5)
 
-    a = 97
-    while (a < 120):
-        display.clear()
-        display.set_character(chr(a), 0)
-        display.set_character(chr(a + 1), 1)
-        display.set_character(chr(a + 2), 2)
-        display.set_character(chr(a + 3), 3)
-        display.draw()
-        a += 1
-        time.sleep(0.5)
-    time.sleep(5)
+        a = 97
+        while (a < 120):
+            display.clear()
+            display.set_character(chr(a), 0, point_state)
+            display.set_character(chr(a + 1), 1, point_state)
+            display.set_character(chr(a + 2), 2, point_state)
+            display.set_character(chr(a + 3), 3, point_state)
+            display.draw()
+            a += 1
+            time.sleep(0.5)
+        time.sleep(5)
 
-    a = 0
-    while (a < 7):
-        display.clear()
-        display.set_number(a, 0)
-        display.set_number(a + 1, 1)
-        display.set_number(a + 2, 2)
-        display.set_number(a + 3, 3)
-        display.draw()
-        a += 1
-        time.sleep(0.5)
-    time.sleep(5)
+        a = 0
+        while (a < 7):
+            display.clear()
+            display.set_number(a, 0, point_state)
+            display.set_number(a + 1, 1, point_state)
+            display.set_number(a + 2, 2, point_state)
+            display.set_number(a + 3, 3, point_state)
+            display.draw()
+            a += 1
+            time.sleep(0.5)
+        time.sleep(5)
 
-    a = 63
-    while (a < 73):
-        display.clear()
-        display.set_code(a, 0)
-        display.set_code(a + 1, 1)
-        display.set_code(a + 2, 2)
-        display.set_code(a + 3, 3)
-        display.draw()
-        a += 1
-        time.sleep(0.5)
-    time.sleep(5)
+        a = 63
+        while (a < 73):
+            display.clear()
+            display.set_code(a, 0, point_state)
+            display.set_code(a + 1, 1, point_state)
+            display.set_code(a + 2, 2, point_state)
+            display.set_code(a + 3, 3, point_state)
+            display.draw()
+            a += 1
+            time.sleep(0.5)
+        time.sleep(5)
+        point_state = not point_state
