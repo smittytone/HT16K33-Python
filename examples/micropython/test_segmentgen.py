9c4f84acb0f10bac0769a29c50eed0998cb93121
smittytone <tsmith@korewireless.com>
2023-08-31 10:30:39 +0100

    Test generic code w. example

--- examples/micropython/test_segmentgen.py
+++ examples/micropython/test_segmentgen.py
@@ -1,6 +1,124 @@
-# Import the base class
-from ht16k33 import HT16K33
+class HT16K33:
+    """
+    A simple, generic driver for the I2C-connected Holtek HT16K33 controller chip.
+    This release supports MicroPython and CircuitPython
 
+    Version:    3.5.0
+    Bus:        I2C
+    Author:     Tony Smith (@smittytone)
+    License:    MIT
+    Copyright:  2023
+    """
+
+    # *********** CONSTANTS **********
+
+    HT16K33_GENERIC_DISPLAY_ON = 0x81
+    HT16K33_GENERIC_DISPLAY_OFF = 0x80
+    HT16K33_GENERIC_SYSTEM_ON = 0x21
+    HT16K33_GENERIC_SYSTEM_OFF = 0x20
+    HT16K33_GENERIC_DISPLAY_ADDRESS = 0x00
+    HT16K33_GENERIC_CMD_BRIGHTNESS = 0xE0
+    HT16K33_GENERIC_CMD_BLINK = 0x81
+
+    # *********** PRIVATE PROPERTIES **********
+
+    i2c = None
+    address = 0
+    brightness = 15
+    flash_rate = 0
+
+    # *********** CONSTRUCTOR **********
+
+    def __init__(self, i2c, i2c_address):
+        assert 0x00 <= i2c_address < 0x80, "ERROR - Invalid I2C address in HT16K33()"
+        self.i2c = i2c
+        self.address = i2c_address
+        self.power_on()
+
+    # *********** PUBLIC METHODS **********
+
+    def set_blink_rate(self, rate=0):
+        """
+        Set the display's flash rate.
+
+        Only four values (in Hz) are permitted: 0, 2, 1, and 0,5.
+
+        Args:
+            rate (int): The chosen flash rate. Default: 0Hz (no flash).
+        """
+        assert rate in (0, 0.5, 1, 2), "ERROR - Invalid blink rate set in set_blink_rate()"
+        self.blink_rate = rate & 0x03
+        self._write_cmd(self.HT16K33_GENERIC_CMD_BLINK | rate << 1)
+
+    def set_brightness(self, brightness=15):
+        """
+        Set the display's brightness (ie. duty cycle).
+
+        Brightness values range from 0 (dim, but not off) to 15 (max. brightness).
+
+        Args:
+            brightness (int): The chosen flash rate. Default: 15 (100%).
+        """
+        if brightness < 0 or brightness > 15: brightness = 15
+        self.brightness = brightness
+        self._write_cmd(self.HT16K33_GENERIC_CMD_BRIGHTNESS | brightness)
+
+    def draw(self):
+        """
+        Writes the current display buffer to the display itself.
+
+        Call this method after updating the buffer to update
+        the LED itself.
+        """
+        self._render()
+
+    def update(self):
+        """
+        Alternative for draw() for backwards compatibility
+        """
+        self._render()
+
+    def clear(self):
+        """
+        Clear the buffer.
+
+        Returns:
+            The instance (self)
+        """
+        for i in range(0, len(self.buffer)): self.buffer[i] = 0x00
+        return self
+
+    def power_on(self):
+        """
+        Power on the controller and display.
+        """
+        self._write_cmd(self.HT16K33_GENERIC_SYSTEM_ON)
+        self._write_cmd(self.HT16K33_GENERIC_DISPLAY_ON)
+
+    def power_off(self):
+        """
+        Power on the controller and display.
+        """
+        self._write_cmd(self.HT16K33_GENERIC_DISPLAY_OFF)
+        self._write_cmd(self.HT16K33_GENERIC_SYSTEM_OFF)
+
+    # ********** PRIVATE METHODS **********
+
+    def _render(self):
+        """
+        Write the display buffer out to I2C
+        """
+        buffer = bytearray(len(self.buffer) + 1)
+        buffer[1:] = self.buffer
+        buffer[0] = 0x00
+        self.i2c.writeto(self.address, bytes(buffer))
+
+    def _write_cmd(self, byte):
+        """
+        Writes a single command to the HT16K33. A private method.
+        """
+        self.i2c.writeto(self.address, bytes([byte]))
+        
 class HT16K33SegmentGen(HT16K33):
     """
     Micro/Circuit Python class for a generic 1-8-digit, 7-segment display.
@@ -170,3 +288,69 @@ class HT16K33SegmentGen(HT16K33):
                 a &= 0xC0
                 self.buffer[i << 1] = (a | b | c)
         self._render()
+        
+ # IMPORTS
+import utime as time
+from machine import I2C, Pin, RTC
+
+# CONSTANTS
+DELAY = 0.01
+PAUSE = 3
+
+# START
+if __name__ == '__main__':
+    # Delete or comment out all but one of the following i2c instantiations
+    i2c = I2C(1, scl=Pin(3), sda=Pin(2))  # Adafruit QTPy RP2040
+
+    display = HT16K33SegmentGen(i2c, digits=4)
+    display.set_brightness(2)
+
+    # Write 'SYNC' to the LED using custom glyphs
+    sync_text = b"\x6D\x6E\x37\x39"
+    for i in range(len(sync_text)):
+        display.set_glyph(sync_text[i], i)
+    display.draw()
+    time.sleep(PAUSE)
+
+    # Write 'SYNC' to the LED -- this time with decimal points
+    sync_text = b"\x6D\x6E\x37\x39"
+    for i in range(len(sync_text)):
+        display.set_glyph(sync_text[i], i, True)
+    display.draw()
+    time.sleep(PAUSE)
+
+    # Write 'BEEF' to the display using the charset characters
+    display.set_character("B", 0).set_character("E", 1)
+    display.set_character("E", 2).set_character("F", 3)
+    display.draw()
+    time.sleep(PAUSE)
+
+    display.set_character(" ", 0).set_character(" ", 3).draw()
+    time.sleep(PAUSE)
+
+    # Show a countdown using the charset numbers
+    # (also uses 'set_colon()')
+    count = 1100
+    colon_state = True
+    while True:
+        # Convert 'count' into Binary-Coded Decimal (BCD)
+        bcd = int(str(count), 16)
+
+        # Display 'count' as decimal digits
+        display.set_number((bcd & 0xF000) >> 12, 0, colon_state)
+        display.set_number((bcd & 0x0F00) >> 8, 1, colon_state)
+        display.set_number((bcd & 0xF0) >> 4, 2, colon_state)
+        display.set_number((bcd & 0x0F), 3, colon_state)
+
+        if count % 10 == 0: colon_state = not colon_state
+        display.update()
+
+        count -= 1
+        if count < 0: break
+
+        # Pause for breath
+        time.sleep(DELAY)
+
+    # Flash the LED
+    display.set_blink_rate(1)
+    
