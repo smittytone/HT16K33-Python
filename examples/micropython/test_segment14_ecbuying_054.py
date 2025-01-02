69657a335a1fa44f6c51afa6280cb579c70b082e
Tony Smith <smittytone@icloud.com>
2025-01-02 10:55:00 +0000

    Add EC Buying examples

---
+++ examples/micropython/test_segment14_ecbuying_054.py
@@ -0,0 +1,70 @@
+# IMPORTS
+import utime as time
+from machine import I2C, Pin, RTC
+from ht16k33 import HT16K33Segment14
+
+# CONSTANTS
+DELAY = 0.01
+PAUSE = 3
+
+# START
+if __name__ == '__main__':
+    # Configured for the Raspberry Pi Pico -- update for your own setup
+    i2c = I2C(0,scl=Pin(9),sda=Pin(8))
+    display = HT16K33Segment14(i2c,board=HT16K33Segment14.ECBUYING_054)
+    display.set_brightness(2)
+    display.clear()
+
+    point_state = True
+    while True:
+        a = 65
+        while (a < 88):
+            display.clear()
+            display.set_character(chr(a), 0, point_state)
+            display.set_character(chr(a + 1), 1, point_state)
+            display.set_character(chr(a + 2), 2, point_state)
+            display.set_character(chr(a + 3), 3, point_state)
+            display.draw()
+            a += 1
+            time.sleep(0.5)
+        time.sleep(5)
+
+        a = 97
+        while (a < 120):
+            display.clear()
+            display.set_character(chr(a), 0, point_state)
+            display.set_character(chr(a + 1), 1, point_state)
+            display.set_character(chr(a + 2), 2, point_state)
+            display.set_character(chr(a + 3), 3, point_state)
+            display.draw()
+            a += 1
+            time.sleep(0.5)
+        time.sleep(5)
+
+        a = 0
+        while (a < 7):
+            display.clear()
+            display.set_number(a, 0, point_state)
+            display.set_number(a + 1, 1, point_state)
+            display.set_number(a + 2, 2, point_state)
+            display.set_number(a + 3, 3, point_state)
+            display.draw()
+            a += 1
+            time.sleep(0.5)
+        time.sleep(5)
+
+        a = 63
+        while (a < 73):
+            display.clear()
+            display.set_code(a, 0, point_state)
+            display.set_code(a + 1, 1, point_state)
+            display.set_code(a + 2, 2, point_state)
+            display.set_code(a + 3, 3, point_state)
+            display.draw()
+            a += 1
+            time.sleep(0.5)
+        time.sleep(5)
+        point_state = not point_state
+
+        # Check that the following call fails correctly on ADAFRUIT_054, etc.
+        assert display.set_colon(true) == display, "ERROR - display.set_colon() not returning self"
